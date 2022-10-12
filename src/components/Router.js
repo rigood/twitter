efc25cbabc5462c6fcadd9db9b65d5be3da97aea
rigood <rigood@naver.com>
2022-10-12 13:57:03 +0900

    feat: logout
    * feat: navigation menu
    
    * fix: react-router-dom rendering issue by moving React.StrictMode inside the Router.

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,24 +1,35 @@
 import React from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "../routes/Home";
-import Auth from "../routes/Auth";
+import { HashRouter as Router, Redirect, Route, Switch } from "react-router-dom";
+import Home from "routes/Home";
+import Auth from "routes/Auth";
+import Profile from "routes/Profile";
+import Navigation from "components/Navigation";
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
-      <Switch>
-        {isLoggedIn ? (
-          <>
-            <Route exact path="/">
-              <Home />
-            </Route>
-          </>
-        ) : (
-          <Route exact path="/">
-            <Auth />
-          </Route>
-        )}
-      </Switch>
+      <React.StrictMode>
+        {isLoggedIn && <Navigation />}
+        <Switch>
+          {isLoggedIn ? (
+            <>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route exact path="/profile">
+                <Profile />
+              </Route>
+            </>
+          ) : (
+            <>
+              <Route exact path="/">
+                <Auth />
+              </Route>
+              <Redirect from="*" to="/" />
+            </>
+          )}
+        </Switch>
+      </React.StrictMode>
     </Router>
   );
 };
