4bdd776f9f4f0edcd1a44edbb4651381a8021e87
rigood <rigood@naver.com>
2022-10-12 16:28:21 +0900

    feat: img preview

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { dbService } from "fbase";
 import Tweet from "components/Tweet";
 
 const Home = ({ userObj }) => {
   const [tweet, setTweet] = useState("");
   const [tweets, setTweets] = useState([]);
+  const [attachment, setAttachment] = useState(null);
   //   const getTweets = async () => {
   //     const dbTweets = await dbService.collection("tweets").get();
   //     dbTweets.forEach((document) => {
@@ -40,12 +41,40 @@ const Home = ({ userObj }) => {
     } = event;
     setTweet(value);
   };
+  const onFileChange = (event) => {
+    const {
+      target: { files },
+    } = event;
+    const theFile = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setAttachment(result);
+    };
+    if (theFile) {
+      reader.readAsDataURL(theFile);
+    }
+  };
+  const fileInput = useRef();
+  const onClearAttachment = () => {
+    setAttachment(null);
+    fileInput.current.value = null;
+  };
   return (
     <>
       <div>
         <form onSubmit={onSubmit}>
           <input value={tweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
+          <input type="file" accept="image/*" onChange={onFileChange} ref={fileInput} />
           <input type="submit" value="Tweet" />
+          {attachment && (
+            <div>
+              <img src={attachment} width="50px" height="50px" />
+              <button onClick={onClearAttachment}>Clear</button>
+            </div>
+          )}
         </form>
       </div>
       <div>
