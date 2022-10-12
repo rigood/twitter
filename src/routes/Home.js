8c5b647da12658b448f00efb60270a8fdf06d850
rigood <rigood@naver.com>
2022-10-12 14:58:07 +0900

    feat: realtime tweet
    * feat: add creatorId
    
    * chore: remove manifest link

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,27 +1,35 @@
 import React, { useEffect, useState } from "react";
 import { dbService } from "fbase";
 
-const Home = () => {
+const Home = ({ userObj }) => {
   const [tweet, setTweet] = useState("");
   const [tweets, setTweets] = useState([]);
-  const getTweets = async () => {
-    const dbTweets = await dbService.collection("tweets").get();
-    dbTweets.forEach((document) => {
-      const tweetObject = {
-        ...document.data(),
-        id: document.id,
-      };
-      setTweets((prev) => [tweetObject, ...prev]);
-    });
-  };
+  //   const getTweets = async () => {
+  //     const dbTweets = await dbService.collection("tweets").get();
+  //     dbTweets.forEach((document) => {
+  //       const tweetObject = {
+  //         ...document.data(),
+  //         id: document.id,
+  //       };
+  //       setTweets((prev) => [tweetObject, ...prev]);
+  //     });
+  //   };
   useEffect(() => {
-    getTweets();
+    // getTweets();
+    dbService
+      .collection("tweets")
+      .orderBy("createdAt", "desc")
+      .onSnapshot((snapshot) => {
+        const tweetArray = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+        setTweets(tweetArray);
+      });
   }, []);
   const onSubmit = async (event) => {
     event.preventDefault();
     await dbService.collection("tweets").add({
-      tweet,
+      text: tweet,
       createdAt: Date.now(),
+      creatorId: userObj.uid,
     });
     setTweet("");
   };
@@ -43,7 +51,7 @@ const Home = () => {
       <div>
         {tweets.map((tweet) => (
           <div key={tweet.id}>
-            <h4>{tweet.tweet}</h4>
+            <h4>{tweet.text}</h4>
           </div>
         ))}
       </div>
