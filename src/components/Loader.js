f9399ec8a183baa123015b76dc7dc98566c283e2
rigood <rigood@naver.com>
2023-01-15 15:00:20 +0900

    Style: Loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,5 +1,10 @@
 function Loader() {
-  return <div>Loading</div>;
+  return (
+    <div className="loader">
+      <img className="loader-img" src="favicon.ico" alt="icon"></img>
+      <div className="loader-text">사용자 정보를 불러오는 중</div>
+    </div>
+  );
 }
 
 export default Loader;
