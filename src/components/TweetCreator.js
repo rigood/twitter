d57e2ec19b134cef28a28679a3283b739663463f
rigood <rigood@naver.com>
2023-01-15 10:33:04 +0900

    Refactor: split Home component

---
+++ src/components/TweetCreator.js
@@ -0,0 +1,88 @@
+import { useState, useRef } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { dbService, storageService } from "fbase";
+
+function TweetCreator({ userObj }) {
+  const [tweet, setTweet] = useState("");
+  const [attachment, setAttachment] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    let attachmentUrl = "";
+    if (attachment !== "") {
+      const attachmentRef = storageService
+        .ref()
+        .child(`${userObj.uid}/${uuidv4()}`);
+      const response = await attachmentRef.putString(attachment, "data_url");
+      attachmentUrl = await response.ref.getDownloadURL();
+    }
+    const tweetObj = {
+      text: tweet,
+      createdAt: Date.now(),
+      creatorId: userObj.uid,
+      attachmentUrl,
+    };
+    await dbService.collection("tweets").add(tweetObj);
+    setTweet("");
+    setAttachment("");
+    fileInput.current.value = "";
+  };
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setTweet(value);
+  };
+
+  const onFileChange = (event) => {
+    const {
+      target: { files },
+    } = event;
+    const theFile = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setAttachment(result);
+    };
+    if (theFile) {
+      reader.readAsDataURL(theFile);
+    }
+  };
+
+  const fileInput = useRef();
+
+  const onClearAttachment = () => {
+    setAttachment("");
+    fileInput.current.value = "";
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        value={tweet}
+        onChange={onChange}
+        type="text"
+        placeholder="What's on your mind?"
+        maxLength={120}
+      />
+      <input
+        type="file"
+        accept="image/*"
+        onChange={onFileChange}
+        ref={fileInput}
+      />
+      <input type="submit" value="Tweet" />
+      {attachment && (
+        <div>
+          <img src={attachment} width="50px" height="50px" />
+          <button onClick={onClearAttachment}>Clear</button>
+        </div>
+      )}
+    </form>
+  );
+}
+
+export default TweetCreator;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,24 +1,12 @@
-import React, { useEffect, useState, useRef } from "react";
-import { v4 as uuidv4 } from "uuid";
-import { dbService, storageService } from "fbase";
+import React, { useEffect, useState } from "react";
+import { dbService } from "fbase";
 import Tweet from "components/Tweet";
+import TweetCreator from "components/TweetCreator";
 
 const Home = ({ userObj }) => {
-  const [tweet, setTweet] = useState("");
   const [tweets, setTweets] = useState([]);
-  const [attachment, setAttachment] = useState("");
-  //   const getTweets = async () => {
-  //     const dbTweets = await dbService.collection("tweets").get();
-  //     dbTweets.forEach((document) => {
-  //       const tweetObject = {
-  //         ...document.data(),
-  //         id: document.id,
-  //       };
-  //       setTweets((prev) => [tweetObject, ...prev]);
-  //     });
-  //   };
+
   useEffect(() => {
-    // getTweets();
     dbService
       .collection("tweets")
       .orderBy("createdAt", "desc")
@@ -30,81 +18,11 @@ const Home = ({ userObj }) => {
         setTweets(tweetArray);
       });
   }, []);
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    let attachmentUrl = "";
-    if (attachment !== "") {
-      const attachmentRef = storageService
-        .ref()
-        .child(`${userObj.uid}/${uuidv4()}`);
-      const response = await attachmentRef.putString(attachment, "data_url");
-      attachmentUrl = await response.ref.getDownloadURL();
-    }
-    const tweetObj = {
-      text: tweet,
-      createdAt: Date.now(),
-      creatorId: userObj.uid,
-      attachmentUrl,
-    };
-    await dbService.collection("tweets").add(tweetObj);
-    setTweet("");
-    setAttachment("");
-    fileInput.current.value = "";
-  };
-  const onChange = (event) => {
-    const {
-      target: { value },
-    } = event;
-    setTweet(value);
-  };
-  const onFileChange = (event) => {
-    const {
-      target: { files },
-    } = event;
-    const theFile = files[0];
-    const reader = new FileReader();
-    reader.onloadend = (finishedEvent) => {
-      const {
-        currentTarget: { result },
-      } = finishedEvent;
-      setAttachment(result);
-    };
-    if (theFile) {
-      reader.readAsDataURL(theFile);
-    }
-  };
-  const fileInput = useRef();
-  const onClearAttachment = () => {
-    setAttachment("");
-    fileInput.current.value = "";
-  };
+
   return (
     <>
-      <div>
-        <form onSubmit={onSubmit}>
-          <input
-            value={tweet}
-            onChange={onChange}
-            type="text"
-            placeholder="What's on your mind?"
-            maxLength={120}
-          />
-          <input
-            type="file"
-            accept="image/*"
-            onChange={onFileChange}
-            ref={fileInput}
-          />
-          <input type="submit" value="Tweet" />
-          {attachment && (
-            <div>
-              <img src={attachment} width="50px" height="50px" />
-              <button onClick={onClearAttachment}>Clear</button>
-            </div>
-          )}
-        </form>
-      </div>
-      <div>
+      <>
+        <TweetCreator userObj={userObj} />
         {tweets.map((tweet) => (
           <Tweet
             key={tweet.id}
@@ -112,7 +30,7 @@ const Home = ({ userObj }) => {
             isOwner={tweet.creatorId === userObj.uid}
           />
         ))}
-      </div>
+      </>
     </>
   );
 };
