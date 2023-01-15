955d9e5eae82995f136de62a6c4d9d8e7de457ea
rigood <rigood@naver.com>
2023-01-16 02:57:49 +0900

    fix: fix Navigation logout logic

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,12 @@
-import { useHistory, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { authService } from "fbase";
 
 function Navigation() {
-  const history = useHistory();
-  const onLogOutClick = () => {
+  const onLogOutClick = (event) => {
+    event.preventDefault();
     const ok = window.confirm("로그아웃 하시겠습니까?");
     if (ok) {
       authService.signOut();
-      history.push("/");
     }
   };
 
@@ -16,15 +15,15 @@ function Navigation() {
       <ul>
         <li className="nav-left">
           <Link to="/" title="홈으로 이동">
-            <i class="fa-brands fa-twitter"></i>
+            <i className="fa-brands fa-twitter"></i>
           </Link>
         </li>
         <li className="nav-right">
           <Link to="/profile" title="프로필로 이동">
-            <i class="fa-solid fa-user"></i>
+            <i className="fa-solid fa-user"></i>
           </Link>
-          <Link onClick={onLogOutClick} title="로그아웃">
-            <i class="fa-solid fa-right-from-bracket"></i>
+          <Link to="/" onClick={onLogOutClick} title="로그아웃">
+            <i className="fa-solid fa-right-from-bracket"></i>
           </Link>
         </li>
       </ul>
