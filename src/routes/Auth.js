0d622ee4facd652652d1160060eb67d831129f74
rigood <rigood@naver.com>
2023-01-15 11:13:19 +0900

    Refactor: split Auth component

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,35 +1,7 @@
-import React, { useState } from "react";
 import { authService, firebaseInstance } from "fbase";
+import AuthForm from "components/AuthForm";
 
 const Auth = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [newAccount, setNewAccount] = useState(true);
-  const [error, setError] = useState("");
-  const onChange = (event) => {
-    const {
-      target: { name, value },
-    } = event;
-    if (name === "email") {
-      setEmail(value);
-    } else if (name === "password") {
-      setPassword(value);
-    }
-  };
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      let data;
-      if (newAccount) {
-        data = await authService.createUserWithEmailAndPassword(email, password);
-      } else {
-        data = await authService.signInWithEmailAndPassword(email, password);
-      }
-    } catch (error) {
-      setError(error.message);
-    }
-  };
-  const toggleAccount = () => setNewAccount((prev) => !prev);
   const onSocialClick = async (event) => {
     const {
       target: { name },
@@ -40,16 +12,12 @@ const Auth = () => {
     } else if (name === "github") {
       provider = new firebaseInstance.auth.GithubAuthProvider();
     }
-    const data = await authService.signInWithPopup(provider);
+    await authService.signInWithPopup(provider);
   };
+
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input name="email" type="email" placeholder="Email" required value={email} onChange={onChange} />
-        <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange} />
-        <input type="submit" value={newAccount ? "Create Account" : "Log In"} required />
-      </form>
-      <span onClick={toggleAccount}>{newAccount ? "Sign In" : "Create Account"}</span>
+    <>
+      <AuthForm />
       <div>
         <button onClick={onSocialClick} name="google">
           Continue with Google
@@ -57,9 +25,8 @@ const Auth = () => {
         <button onClick={onSocialClick} name="github">
           Continue with Github
         </button>
-        {error}
       </div>
-    </div>
+    </>
   );
 };
 
