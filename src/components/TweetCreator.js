e6168282d1a6654058b4de08d755efdae745780f
rigood <rigood@naver.com>
2023-01-16 15:33:51 +0900

    style: split TweetCreator component and add styles

--- src/components/TweetCreator.js
+++ src/components/TweetCreator.js
@@ -4,7 +4,46 @@ import { dbService, storageService } from "fbase";
 
 function TweetCreator({ userObj }) {
   const [tweet, setTweet] = useState("");
+  const [isLengthValid, setIsLengthValid] = useState(false);
   const [attachment, setAttachment] = useState("");
+
+  const textareaRef = useRef(null);
+  const fileInputRef = useRef(null);
+
+  const onChange = (event) => {
+    textareaRef.current.style.height = "auto";
+    textareaRef.current.style.height = textareaRef.current.scrollHeight + "px";
+    const {
+      target: { value },
+    } = event;
+    if (value !== "") {
+      setIsLengthValid(true);
+    } else {
+      setIsLengthValid(false);
+    }
+    setTweet(value);
+  };
+
+  const onFileClick = () => {
+    fileInputRef.current?.click();
+  };
+
+  const onFileChange = (event) => {
+    const {
+      target: { files },
+    } = event;
+    const theFile = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setAttachment(result);
+    };
+    if (theFile) {
+      reader.readAsDataURL(theFile);
+    }
+  };
 
   const onSubmit = async (event) => {
     event.preventDefault();
@@ -25,62 +64,64 @@ function TweetCreator({ userObj }) {
     await dbService.collection("tweets").add(tweetObj);
     setTweet("");
     setAttachment("");
-    fileInput.current.value = "";
+    fileInputRef.current.value = "";
   };
-
-  const onChange = (event) => {
-    const {
-      target: { value },
-    } = event;
-    setTweet(value);
-  };
-
-  const onFileChange = (event) => {
-    const {
-      target: { files },
-    } = event;
-    const theFile = files[0];
-    const reader = new FileReader();
-    reader.onloadend = (finishedEvent) => {
-      const {
-        currentTarget: { result },
-      } = finishedEvent;
-      setAttachment(result);
-    };
-    if (theFile) {
-      reader.readAsDataURL(theFile);
-    }
-  };
-
-  const fileInput = useRef();
 
   const onClearAttachment = () => {
     setAttachment("");
-    fileInput.current.value = "";
+    fileInputRef.current.value = "";
   };
 
   return (
-    <form onSubmit={onSubmit}>
-      <input
-        value={tweet}
-        onChange={onChange}
-        type="text"
-        placeholder="What's on your mind?"
-        maxLength={120}
-      />
-      <input
-        type="file"
-        accept="image/*"
-        onChange={onFileChange}
-        ref={fileInput}
-      />
-      <input type="submit" value="Tweet" />
-      {attachment && (
-        <div>
-          <img src={attachment} width="50px" height="50px" />
-          <button onClick={onClearAttachment}>Clear</button>
+    <form onSubmit={onSubmit} className="creator-form">
+      <div className="creator-profile">
+        <img src="assets/default-profile.jpg" />
+      </div>
+      <div className="creator-write">
+        <textarea
+          value={tweet}
+          onChange={onChange}
+          placeholder="What's on your mind?"
+          maxLength={120}
+          required
+          ref={textareaRef}
+        />
+        <input
+          type="file"
+          accept="image/*"
+          onChange={onFileChange}
+          ref={fileInputRef}
+          hidden
+        />
+        {attachment && (
+          <div className="creator-write__preview">
+            <img src={attachment} />
+            <button onClick={onClearAttachment} className="creator-clear">
+              <i className="fa-solid fa-xmark"></i>
+            </button>
+          </div>
+        )}
+        <div className="creator-write__toolbar">
+          <div className="creator-write__toolbar-left">
+            <button
+              type="button"
+              className="creator-button"
+              onClick={onFileClick}
+            >
+              <i className="fa-solid fa-image"></i>
+            </button>
+          </div>
+          <div className="creator-write__toolbar-right">
+            <span className="creator-length">{tweet.length}/120</span>
+            <input
+              type="submit"
+              value="Tweet"
+              className="creator-submit"
+              disabled={!isLengthValid}
+            />
+          </div>
         </div>
-      )}
+      </div>
     </form>
   );
 }
