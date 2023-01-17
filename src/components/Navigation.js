c6ba4b4d127f9ecbf27b56319d30c5159aaaab07
rigood <rigood@naver.com>
2023-01-17 20:46:34 +0900

    feat: 현재 페이지 NavLink 스타일

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,8 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { authService } from "fbase";
 
 function Navigation() {
-  const onLogOutClick = (event) => {
-    event.preventDefault();
+  const onLogOutClick = () => {
     const ok = window.confirm("로그아웃 하시겠습니까?");
     if (ok) {
       authService.signOut();
@@ -14,17 +13,26 @@ function Navigation() {
     <nav className="nav">
       <ul>
         <li className="nav-left">
-          <Link to="/" title="홈으로 이동">
+          <NavLink
+            exact
+            to="/"
+            title="홈으로 이동"
+            activeClassName="nav-active"
+          >
             <i className="fa-brands fa-twitter"></i>
-          </Link>
+          </NavLink>
         </li>
         <li className="nav-right">
-          <Link to="/profile" title="프로필로 이동">
+          <NavLink
+            to="/profile"
+            title="프로필로 이동"
+            activeClassName="nav-active"
+          >
             <i className="fa-solid fa-user"></i>
-          </Link>
-          <Link to="/" onClick={onLogOutClick} title="로그아웃">
+          </NavLink>
+          <a onClick={onLogOutClick} title="로그아웃">
             <i className="fa-solid fa-right-from-bracket"></i>
-          </Link>
+          </a>
         </li>
       </ul>
     </nav>
